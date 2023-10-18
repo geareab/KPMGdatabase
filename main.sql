7fff3e2dcc6133a1b25a7ef81ec7b9ab7eb771a2
Uday Singh <60573564+geareab@users.noreply.github.com>
2023-10-18 23:36:00 +0200

    Final main.sql

--- main.sql
+++ main.sql
@@ -1,21 +1,25 @@
-SELECT E.FName AS "Employee Name", E.LName AS "Employee Last Name",
-       M.FName AS "Manager Name",  M.LName AS "Manager Last Name", E.Salary AS "Salary",E.town AS "Town"
+SELECT E.FName AS "FName", E.LName AS "LName",
+       M.FName AS "SupervisorFName",  M.LName AS "SupervisoLName", E.Salary AS "Salary", E.town AS "Town"
 FROM Employee E
 LEFT OUTER JOIN Employee M
 ON M.ID = E.SupervisedBy
 WHERE E.Salary > 30000
 ORDER BY E.town ASC;
 
+
 SELECT Town, COUNT(*) as Count_Employee
 FROM Employee
 GROUP BY Town
 HAVING COUNT(*) > 10;
-SELECT E.FName AS "Employee Name", E.LName AS "Employee Last Name",
-       M.FName AS "Manager Name",  M.LName AS "Manager Last Name", E.Salary AS "salary"
+
+
+SELECT E.FName AS "FName", E.LName AS "LName",
+       M.FName AS "SupervisorFName",  M.LName AS "SupervisoLName", E.Salary AS "Salary", E.town AS "Town"
 FROM Employee E
 LEFT OUTER JOIN Employee M
 ON M.ID = E.SupervisedBy
 WHERE E.Salary = (SELECT MIN(Salary) FROM Employee);
+
 
 Select * from Employee
 ORDER BY ID ASC;
